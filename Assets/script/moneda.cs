da93461ae7093990da54725a3bfe0d8458e1e667
Carlos Castro Lozano <carlos.c.l.yo@gmail.com>
2016-11-09 18:21:24 +0100

    Prefab moneda
    script, material y prefab

---
+++ Assets/script/moneda.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moneda : MonoBehaviour {
+private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		//Destroy (GameObject, 3);
+		//Random.Range (0,10);
+		rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (new Vector2 (Random.Range(-200,200), 200));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+}
