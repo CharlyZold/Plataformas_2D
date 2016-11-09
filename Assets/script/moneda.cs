39d6bb9eb514cc7eaff017ad260d40a82a34a4ac
Carlos Castro Lozano <carlos.c.l.yo@gmail.com>
2016-11-09 18:44:17 +0100

    Generador_moneda
    generador moneda y caja

--- Assets/script/moneda.cs
+++ Assets/script/moneda.cs
@@ -5,7 +5,7 @@ public class moneda : MonoBehaviour {
 private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		//Destroy (GameObject, 3);
+		Destroy (gameObject, 3);
 		//Random.Range (0,10);
 		rb = GetComponent<Rigidbody2D> ();
 		rb.AddForce (new Vector2 (Random.Range(-200,200), 200));
