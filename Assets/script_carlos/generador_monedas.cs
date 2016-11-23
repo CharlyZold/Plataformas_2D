4aa9b740e236755b0243d306b738d0fd3e5c41cc
Carlos Castro <carlos.c.l.yo@gmail.com>
2016-11-23 18:37:21 +0100

    Generador de monedas se desactiva a los 3 golpes

--- Assets/script_carlos/generador_monedas.cs
+++ Assets/script_carlos/generador_monedas.cs
@@ -8,6 +8,7 @@ public class generador_monedas : MonoBehaviour {
 	//private int numero_monedas=0;
 	public Animator anim;
 	private bool collision = false;
+	private int ani=0;
 
 	void start(){
 		pos_salida = transform.Find ("posicion_salida").transform;	
@@ -21,15 +22,24 @@ public class generador_monedas : MonoBehaviour {
 	//	}
 	//}
 
+
 	void OnTriggerEnter2D(Collider2D objeto){
-		if (objeto.tag == "Player" && moneda_nueva == null) {
+		
+		if (objeto.tag == "Player" && moneda_nueva == null && ani <= 2) {
 			moneda_nueva = (GameObject)Instantiate (Monedas[Random.Range (0, Monedas.Length)], pos_salida.position, transform.rotation);
-			//numero_monedas = Random.Range (0, Monedas.Length-1);
+			//numero_monedas = Random.Range (0, Monedas.Length);
+			anim.SetBool ("collision", true);
+			ani = ani + 1;
+		}
+		if (objeto.tag == "Player" && moneda_nueva == null && ani == 3) {
 			anim.SetBool ("collision", true);
 		}
 	}
 	void OnTriggerExit2D(Collider2D objeto){
 		if (objeto.tag == "Player") {
+			anim.SetBool ("collision", false);
+		}
+		if (objeto.tag == "Player" && moneda_nueva == null && ani == 3) {
 			anim.SetBool ("collision", false);
 		}
 	}
